1ce364e0db4ea0cc80a4d133800180606a3fe2b6
george.papadakis <george.papadakis@oddschecker.com>
2022-03-08 15:14:47 +0200

    Third part of the course

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const {MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT} = require("./config/config");
+
+const postRouter = require('./routes/postRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.get("/", (req, res) => res.send("<h2>Oh hi Mark!!!!!!!!</h2>"));
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+    mongoose.connect(mongoURL)
+        .then(() => console.log('Successfully connected to the database'))
+        .catch((e) => {
+            console.log(e);
+            setTimeout(connectWithRetry, 5000)
+        });
+};
+
+connectWithRetry();
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("<h2>Oh hi Mark!!</h2>"));
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
